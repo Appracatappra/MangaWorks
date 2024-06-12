6190252a570b76b0ed29b507d19a77769b3d687f
Kevin Mullins <kevin_mullins@appracatappra.com>
2024-06-12 10:05:18 -0500

    Swift 6
    Swift 6 and .iOS(.v18), .tvOS(.v18)

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "MangaWorks",
-    platforms: [.iOS(.v17), .tvOS(.v17)],
+    platforms: [.iOS(.v18), .tvOS(.v18)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,16 +15,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Appracatappra/LogManager", .upToNextMajor(from: "1.0.1")),
-        .package(url: "https://github.com/Appracatappra/SwiftletUtilities", .upToNextMajor(from: "1.1.1")),
-        .package(url: "https://github.com/Appracatappra/SoundManager", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/Appracatappra/SimpleSerializer", .upToNextMajor(from: "1.0.5")),
-        .package(url: "https://github.com/Appracatappra/GraceLanguage", .upToNextMajor(from: "1.0.3")),
-        .package(url: "https://github.com/Appracatappra/SwiftUIKit.git", .upToNextMajor(from: "1.0.11")),
-        .package(url: "https://github.com/Appracatappra/SpeechManager", .upToNextMajor(from: "1.0.4")),
-        .package(url: "https://github.com/Appracatappra/SwiftUIGamepad.git", .upToNextMajor(from: "1.0.3")),
-        .package(url: "https://github.com/Appracatappra/SwiftUIPanoramaViewer.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/Appracatappra/ODRManager.git", .upToNextMajor(from: "1.0.1")),
+        .package(url: "https://github.com/Appracatappra/LogManager", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/Appracatappra/SwiftletUtilities", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/Appracatappra/SoundManager", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/Appracatappra/SimpleSerializer", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/Appracatappra/GraceLanguage", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/Appracatappra/SwiftUIKit.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/Appracatappra/SpeechManager", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/Appracatappra/SwiftUIGamepad.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/Appracatappra/SwiftUIPanoramaViewer.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/Appracatappra/ODRManager.git", .upToNextMajor(from: "2.0.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
